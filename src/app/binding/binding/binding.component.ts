c580bc3e53160bb54cf7c723165c33b8ccb8839e
Jatinamin <jatinamin23@gmail.com>
2022-07-01 17:34:19 -0400

    1 file modified, 1 added, 1 deleted

--- src/app/binding/binding/binding.component.ts
+++ src/app/binding/binding/binding.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BindingComponent implements OnInit {
   pageTitle:string="Data Binding In Angular";
   
-  imageUrl:any = "https://media.istockphoto.com/vectors/coniferous-forest-silhouette-template-vector-id543350246?k=20&m=543350246&s=612x612&w=0&h=G4tsv-5HHDcZR1-otpxk9IBtg-cbB795-Xl2NyZDHck=";
+  imageUrl:any = "./app/images/SummerSeason.jpg";
   btnStatus:boolean= false;
   constructor() { }
 
