43a41edd226662043a5c2f1cd1292892cebfec09
Jatinamin <jatinamin23@gmail.com>
2022-07-01 18:53:10 -0400

    1 file modified, 2 deleted

--- src/app/binding/binding/binding.component.ts
+++ src/app/binding/binding/binding.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BindingComponent implements OnInit {
   pageTitle:string="Data Binding In Angular";
   
-  imageUrl:any = "app/images/SummerSeason.jpg" ;
+  imageUrl:any = "https://www.freewebheaders.com/wp-content/gallery/artistic-abstract-hero-headers/thumbs/thumbs_colorful-butterflies-flowers-abstract-art-vector-design.jpg" ;
   btnStatus:boolean= false;
   constructor() { }
 
