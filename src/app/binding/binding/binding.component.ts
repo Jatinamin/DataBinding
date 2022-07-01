eac65509fd67dbc16159c9f507df1e26f7c8c2f3
Jatinamin <jatinamin23@gmail.com>
2022-07-01 19:59:40 -0400

    4 files modified

--- src/app/binding/binding/binding.component.ts
+++ src/app/binding/binding/binding.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindingComponent implements OnInit {
   pageTitle:string="Data Binding In Angular";
-  
+  username:string='';
   imageUrl:string = "https://th.bing.com/th/id/R.17320966d681d03c2e2a79ba6c3776f2?rik=22PtZOqy46yHCw&riu=http%3a%2f%2fblog.entheosweb.com%2fwp-content%2fuploads%2f2012%2f01%2fpink_header_s.jpg&ehk=1uAXrGaPEqsLayRuQ3jF4Dpvehyf9hR7Gh89Xczk69o%3d&risl=&pid=ImgRaw&r=0" ;
   btnStatus:boolean= false;
   constructor() { }
